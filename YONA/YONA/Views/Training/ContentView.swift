ba74e64d2698159e7548fdc7344592acb2293cad
yeni <dev.yeeun@gmail.com>
2023-01-13 23:58:44 +0900

    Add: 스크롤뷰 추가

--- YONA/YONA/Views/Training/ContentView.swift
+++ YONA/YONA/Views/Training/ContentView.swift
@@ -17,28 +17,41 @@ struct ContentView: View {
     private var items: FetchedResults<Item>
 
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+        ScrollView{
+            NavigationView {
+                VStack{
+                    ScrollView(.horizontal){
+                        HStack{
+                            Text("Hello")
+                            Text("World")
+                            Text("Hello")
+                            Text("World")
+                            Text("Hello")
+                        }
+                    }
+                    
+                    List {
+                        ForEach(items) { item in
+                            NavigationLink {
+                                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+                            } label: {
+                                Text(item.timestamp!, formatter: itemFormatter)
+                            }
+                        }
+                        .onDelete(perform: deleteItems)
+                    }
+                    .toolbar {
+                        ToolbarItem(placement: .navigationBarTrailing) {
+                            EditButton()
+                        }
+                        ToolbarItem {
+                            Button(action: addItem) {
+                                Label("Add Item", systemImage: "plus")
+                            }
+                        }
                     }
                 }
-                .onDelete(perform: deleteItems)
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
         }
     }
 
---
+++ YONA/YONA/Views/Training/TrainingListCell.swift
@@ -0,0 +1,20 @@
+//
+//  TrainingListCell.swift
+//  YONA
+//
+//  Created by Yeni Hwang on 2023/01/12.
+//
+
+import SwiftUI
+
+struct TrainingListCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TrainingListCellView_Previews: PreviewProvider {
+    static var previews: some View {
+        TrainingListCell()
+    }
+}
